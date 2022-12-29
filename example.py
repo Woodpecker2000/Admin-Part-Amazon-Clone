6c1ca2990311bf37d03f100b93064392ea73d03f
Woodpecker2000 <111565585+Woodpecker2000@users.noreply.github.com>
2022-12-29 14:38:30 +0000

    f2fs: clean up argument of recover_data

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
 
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
+from typing import Tuple
